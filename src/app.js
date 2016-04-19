788dfe24d60b45bb22bba82992db3240ca44b854
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-04-19 20:00:00 +0700

    first version

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const uuid = require('node-uuid');
 const request = require('request');
+require('dotenv').config();
 
 const REST_PORT = (process.env.PORT || 5000);
 const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
@@ -17,48 +18,168 @@ const sessionIds = new Map();
 
 function processEvent(event) {
     var sender = event.sender.id;
-
+    var text = null;
     if (event.message && event.message.text) {
-        var text = event.message.text;
-        // Handle a text message from this sender
+      text = event.message.text;
+    } else
+    if (event.postback && event.postback.payload ) {
+      text = event.postback.payload
+    }
+    if(text) {
+      // Handle a text message from this sender
 
-        if (!sessionIds.has(sender)) {
-            sessionIds.set(sender, uuid.v1());
-        }
+      if (!sessionIds.has(sender)) {
+          sessionIds.set(sender, uuid.v1());
+      }
 
-        console.log("Text", text);
+      console.log("Text", text);
 
-        let apiaiRequest = apiAiService.textRequest(text,
-            {
-                sessionId: sessionIds.get(sender)
-            });
+      let apiaiRequest = apiAiService.textRequest(text,
+          {
+              sessionId: sessionIds.get(sender)
+          });
 
-        apiaiRequest.on('response', (response) => {
-            if (isDefined(response.result)) {
-                let responseText = response.result.fulfillment.speech;
-                let responseData = response.result.fulfillment.data;
-                let action = response.result.action;
+      apiaiRequest.on('response', (response) => {
+        console.log(response);
 
-                if (isDefined(responseData) && isDefined(responseData.facebook)) {
-                    try {
-                        console.log('Response as formatted message');
-                        sendFBMessage(sender, responseData.facebook);
-                    } catch (err) {
-                        sendFBMessage(sender, {text: err.message });
-                    }
-                } else if (isDefined(responseText)) {
-                    console.log('Response as text message');
+          if (isDefined(response.result)) {
+
+              let responseText = response.result.fulfillment.speech;
+              let source = response.result.fulfillment.source;
+              // if(isDefined(source)){
+              //   sendFBMessage(sender, {text: "wait for a minute!"});
+              //   console.log(sendGenericMessage(JSON.parse(responseText)));
+              // }
+              // let responseData = response.result.fulfillment.data;
+              let action = response.result.action;
+
+              // if (isDefined(responseData) && isDefined(responseData.facebook)) {
+              //     try {
+              //         console.log('Response as formatted message');
+              //         sendFBMessage(sender, responseData.facebook);
+              //     } catch (err) {
+              //         sendFBMessage(sender, {text: err.message });
+              //     }
+              // } else
+              if (isDefined(responseText)) {
+                  console.log('Response as text message');
+                  if(response.result.parameters.down_payment != "" && response.result.parameters.usage == ""){
+                    sendFBMessage(sender, sendButtonMessage(responseText, btnProperties));
+                  }else
+                  if(response.result.parameters.usage != "" && response.result.parameters.property_type == ""){
+                    sendFBMessage(sender, sendButtonMessage(responseText, btnPropertyTypes));
+                  }else
+                  if(action === "loan.purpose"){
+                    sendFBMessage(sender, sendButtonMessage(responseText, btnPurposeTypes));
+                  }else
+                  if(isDefined(source)){
+                    sendFBMessage(sender, sendGenericMessage(JSON.parse(responseText)));
+                  }else {
                     sendFBMessage(sender, {text: responseText});
-                }
+                  }
+              }
 
-            }
-        });
+          }
+      });
 
-        apiaiRequest.on('error', (error) => console.error(error));
-        apiaiRequest.end();
+      apiaiRequest.on('error', function (error) { console.error(error);} );
+      apiaiRequest.end();
     }
 }
+// purpose types
+var btnPurposeTypes = [
+  {
+    "type":"postback",
+    "title":"Purchase",
+    "payload":"purchase"
+  },
+  {
+    "type":"postback",
+    "title":"Refinance",
+    "payload":"refinance"
+  }
+];
+//single family home, duplex, triplex, fourplex, or condo
+var btnProperties = [
+ {
+   "type":"postback",
+   "title":"Primary Residence",
+   "payload":"primary_residence"
+ },
+ {
+   "type":"postback",
+   "title":"Vacation Home",
+   "payload":"vacation_home"
+ },
+ {
+   "type":"postback",
+   "title":"Rental Property",
+   "payload":"rental_property"
+ }
+];
 
+var btnPropertyTypes = [
+ {
+   "type":"postback",
+   "title":"Single Family Home",
+   "payload":"sfh"
+ },
+ {
+   "type":"postback",
+   "title":"Multi-Family",
+   "payload":"multi_family"
+ },
+ {
+   "type":"postback",
+   "title":"Condo/Townhouse",
+   "payload":"condo"
+ }
+];
+
+function sendButtonMessage(text, buttons) {
+  return {
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"button",
+        "text": text,
+        "buttons": buttons
+      }
+    }
+  };
+}
+function sendGenericMessage(messages){
+  var messagesData = {
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"generic",
+        "elements":[]
+      }
+    }
+  };
+  console.log(messages.length);
+  for(var i = 0; i< messages.length;i++){
+
+    var messageData = {
+      "title": messages[i].title,
+      "image_url":messages[i].img_url,
+      "subtitle": messages[i].subtitle,
+      "buttons":[
+        {
+          "type":"web_url",
+          "url":messages[i].url,
+          "title":"Get this rate"
+        }
+      ]
+    };
+    messagesData['attachment']['payload']['elements'][i] = messageData;
+  }
+  return messagesData;
+}
+function test(){
+  var data = '[{"title":"3.350% APR","subtitle":"Monthly Payment: $1,607, Estimated Closing Cost: $1,954","url":"http://localhost:4000/quotes/MColEK3w?program=30yearFixed","type":"30yearFixed"},{"title":"2.750% APR","subtitle":"Monthly Payment: $2,477, Estimated Closing Cost: $1,954","url":"http://localhost:4000/quotes/MColEK3w?program=15yearFixed","type":"15yearFixed"},{"title":"2.500% APR","subtitle":"Monthly Payment: $1,432, Estimated Closing Cost: $1,954","url":"http://localhost:4000/quotes/MColEK3w?program=5yearARM","type":"5yearARM"}]';
+}
 function sendFBMessage(sender, messageData) {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
@@ -114,7 +235,7 @@ app.all('*', function (req, res, next) {
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] == FB_VERIFY_TOKEN) {
         res.send(req.query['hub.challenge']);
-        
+
         setTimeout(function () {
             doSubscribeRequest();
         }, 3000);
@@ -123,7 +244,7 @@ app.get('/webhook/', function (req, res) {
     }
 });
 
-app.post('/webhook/', function (req, res) {
+app.post('/webhook', function (req, res) {
     try {
         var messaging_events = req.body.entry[0].messaging;
         for (var i = 0; i < messaging_events.length; i++) {
