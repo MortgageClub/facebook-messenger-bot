51b75871d872a1ac797264b361d0e4713e19aa63
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-04-22 19:21:16 +0700

    timeout for welcome

--- src/app.js
+++ src/app.js
@@ -118,11 +118,20 @@ function processEvent(event) {
             sendFBMessage(sender, sendTextMessage(arr[0]));
             return;
           }else {
+
             if(arr[0] == welcome ){
-              arr[1] = arr[1].slice(0, 5) + " "+sessionIds.get(sender).context.profile.first_name + arr[1].slice(5);
+              setTimeout(function() {
+                if(sessionIds.get(sender)){
+                  arr[1] = arr[1].slice(0, 5) + " "+sessionIds.get(sender).context.profile.first_name + arr[1].slice(5);
+                  sendFBMessage(sender, sendButtonMessage(arr[1], map.get(arr[0])));
+                }
+              }, 2000);
+              return;
+            }else {
+              sendFBMessage(sender, sendButtonMessage(arr[1], map.get(arr[0])));
+              return;
             }
-            sendFBMessage(sender, sendButtonMessage(arr[1], map.get(arr[0])));
-            return;
+
           }
         }
       }
